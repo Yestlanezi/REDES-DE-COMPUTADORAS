23e8f58f759e129fbe385cd79799f80bea0c8e8f
yestlanezi <yestliibocanegra@gmail.com>
2024-01-21 21:34:44 -0600

    Practicas y tareas

---
+++ capadeenlacededatos.c
@@ -0,0 +1,27 @@
+
+
+//*********Redes************
+//**************************
+
+//21 de septiembre de 2021 
+
+#include<stdio.h> 
+
+void main (){
+	printf("examen dia viernes 24 de septiembre\n");
+	printf("es la unica capa que se divide en dos subcapas\n");
+	printf("--subcapa LLC : habla en el mismo lenguaje con red\n");
+	printf("--subcapa mac (control de acceso al medio): habla el mismo lenguaje que la capa fisica\n");
+	printf("tiene como objetivo preparar los datos\n"); 
+	printf("debe trabajar con la capa superior e inferior\n"); 
+	printf("*********************\n");
+	printf("Creacion de una trama");
+	printf("*********************\n");}
+	printf("se le agrega la informacion, un trailer *para detectar los errores*");
+	printf("----Estandares de la capa 2------\n");
+	printf("");
+
+
+}
+	
+
